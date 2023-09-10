87be932fe2b327b3586b292f029074313bfd5988
SIDDHARTH0760 <79791151+SIDDHARTH0760@users.noreply.github.com>
2023-09-10 17:58:18 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -56,4 +56,58 @@ if st.button('Show Recommendation'):
 
 
 
+# import pickle
+# import streamlit as st
+# import requests
+# import os
+
+# # Get the current directory
+# current_dir = os.getcwd()
+
+# def fetch_poster(movie_id):
+#     url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
+#     data = requests.get(url)
+#     data = data.json()
+#     poster_path = data['poster_path']
+#     full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+#     return full_path
+
+# def recommend(movie):
+#     index = movies[movies['title'] == movie].index[0]
+#     distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+#     recommended_movie_names = []
+#     recommended_movie_posters = []
+#     for i in distances[1:6]:
+#         # fetch the movie poster
+#         movie_id = movies.iloc[i[0]].movie_id
+#         recommended_movie_posters.append(fetch_poster(movie_id))
+#         recommended_movie_names.append(movies.iloc[i[0]].title)
+
+#     return recommended_movie_names,recommended_movie_posters
+
+# st.header('Movie Recommender System')
+
+# # Construct the file paths to the pickle files
+# model_path = os.path.join(current_dir, 'trained_model.sav')
+# similarity_path = os.path.join(current_dir, 'similarity.sav')
+
+# # Load the data from pickle files
+# movies = pickle.load(open(model_path, 'rb'))
+# similarity = pickle.load(open(similarity_path, 'rb'))
+
+# # ...
+
+# if st.button('Show Recommendation'):
+#     recommended_movie_names, recommended_movie_posters = recommend(selected_movie)
+#     col1, col2, col3, col4, col5 = st.beta_columns(5)
+#     with col1:
+#         st.text(recommended_movie_names[0])
+#         st.image(recommended_movie_posters[0])
+#     with col2:
+#         st.text(recommended_movie_names[1])
+#         st.image(recommended_movie_posters[1])
+
+#     # ...
+
+
 
